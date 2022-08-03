a0754733fcf56fb36ff267cfcf825aa0f1119ba6
Farid <bouras.farid@outlook.fr>
2022-08-03 21:24:29 +0200

    feat: added structure pages "favoris"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Comics from './pages/Comics';
+import Favoris from './pages/Favoris';
 import Personnages from './pages/Personnages';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/personnages' element={<Personnages />} />
         <Route path='/comics' element={<Comics />} />
+        <Route path='/favoris' element={<Favoris />} />
         <Route path='*' element={<Personnages />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Favoris.js
@@ -0,0 +1,13 @@
+import Card from '../components/Card';
+import Header from '../components/Header';
+
+const Favoris = () => {
+  return (
+    <div>
+      <Header />
+      <Card />
+    </div>
+  );
+};
+
+export default Favoris;
