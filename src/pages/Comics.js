25d6a55e50b70b15dc53ce4007a2b572438a6e92
Farid <bouras.farid@outlook.fr>
2022-08-03 21:25:21 +0200

    feat: added structure "pages comics"

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -1,5 +1,13 @@
+import Card from '../components/Card';
+import Header from '../components/Header';
+
 const Comics = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Card />
+    </div>
+  );
 };
 
 export default Comics;
