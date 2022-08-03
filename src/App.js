53eda92742f1233f690a84235e8b8fd480e1d033
Farid <bouras.farid@outlook.fr>
2022-08-03 15:37:38 +0200

    feat: architecture general "home"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
