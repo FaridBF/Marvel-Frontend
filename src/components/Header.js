11d090baca0fc3da08e9d57e435d764ed8d59e5c
Farid <bouras.farid@outlook.fr>
2022-08-03 21:25:50 +0200

    feat: added navigation header menu for the 3 pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, useLocation } from 'react-router-dom';
 import '../styles/header.css';
 import logo_marvel from '../assets/marvel_logo.png';
 
 const Header = () => {
+  const location = useLocation();
+  const currentURLPathname = location.pathname;
+
   return (
     <>
       <div className='header-container'>
@@ -12,18 +15,49 @@ const Header = () => {
           </Link>
           <nav>
             <ul>
-              <NavLink
-                to='/personnages'
-                className={(nav) => (nav.isActive ? 'nav-active' : '')}
-              >
-                <li>Accueil</li>
-              </NavLink>
-              <NavLink
-                to='/comics'
-                className={(nav) => (nav.isActive ? 'nav-active' : '')}
-              >
-                <li>Comics</li>
-              </NavLink>
+              {currentURLPathname === '/' && (
+                <>
+                  <NavLink to='/comics'>
+                    <li>Comics</li>
+                  </NavLink>
+                  <NavLink to='/personnages'>
+                    <li>Personnages</li>
+                  </NavLink>
+                  <NavLink to='/favoris'>
+                    <li>Favoris</li>
+                  </NavLink>
+                </>
+              )}
+              {currentURLPathname === '/personnages' && (
+                <>
+                  <NavLink to='/comics'>
+                    <li>Comics</li>
+                  </NavLink>
+                  <NavLink to='/favoris'>
+                    <li>Favoris</li>
+                  </NavLink>
+                </>
+              )}
+              {currentURLPathname === '/favoris' && (
+                <>
+                  <NavLink to='/comics'>
+                    <li>Comics</li>
+                  </NavLink>
+                  <NavLink to='/personnages'>
+                    <li>Personnages</li>
+                  </NavLink>
+                </>
+              )}
+              {currentURLPathname === '/comics' && (
+                <>
+                  <NavLink to='/favoris'>
+                    <li>Favoris</li>
+                  </NavLink>
+                  <NavLink to='/personnages'>
+                    <li>Personnages</li>
+                  </NavLink>
+                </>
+              )}
             </ul>
           </nav>
           <div className='search-container'>
