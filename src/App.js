06650ca71b64152662930140de0670ae6bb73298
Farid <bouras.farid@outlook.fr>
2022-08-03 20:35:08 +0200

    style: added index.css for general rules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Comics from './pages/Comics';
+import Personnages from './pages/Personnages';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='*' element={<Home />} />
+        <Route path='/personnages' element={<Personnages />} />
+        <Route path='/comics' element={<Comics />} />
+        <Route path='*' element={<Personnages />} />
       </Routes>
     </BrowserRouter>
   );
