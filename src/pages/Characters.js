fd9d9e70c697e37deaf4e10b9dc359fc47726af8
Farid <bouras.farid@outlook.fr>
2022-08-04 16:30:02 +0200

    feat: recuperation data ( More precisely : query.title

---
+++ src/pages/Characters.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Card from '../components/Card';
+import Header from '../components/Header';
+import '../styles/loading_spinner.css';
+
+const Personnages = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [input, setInput] = useState('');
+
+  const fetchDataWithInput = async (input) => {
+    try {
+      // const response = await axios.get(
+      //   `https://marvel-backend.herokuapp.com/characters?name=${input}`
+      // );
+      const response =
+        await axios.get(`http://localhost:3000/characters?name=${input}
+      `);
+      setData(response.data.results);
+      console.log(response.data.results);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        //   const response =
+        //     await axios.get(`https://marvel-backend.herokuapp.com/characters
+        // `);
+        const response =
+          await axios.get(`http://localhost:3000/characters?name=${input}
+      `);
+        setData(response.data.results);
+        console.log(response.data.results);
+        setIsLoading(false);
+      };
+      fetchData();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (input.length > 0) {
+      try {
+        fetchDataWithInput(input);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  }, [input]);
+
+  return isLoading === true ? (
+    <div className='animation'>
+      <span className='loader loader_spinner'></span>
+    </div>
+  ) : (
+    <div>
+      <Header setInput={setInput} input={input} />
+
+      {data.map((element, index) => {
+        return <Card key={index} element={element} />;
+      })}
+    </div>
+  );
+};
+
+export default Personnages;
