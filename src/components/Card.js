5674ffcab7c83be17b8570caea2dfabedf2ad342
Farid <bouras.farid@outlook.fr>
2022-08-04 16:34:17 +0200

    feat: creation a one condition for the title and the name int he component card (display different according the current pathname)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,25 @@
 import '../styles/card.css';
+import { useLocation } from 'react-router-dom';
 
-const Card = ({ comic }) => {
+const Card = ({ element }) => {
+  const location = useLocation();
+  const currentURLPathname = location.pathname;
+
   return (
     <div className='card'>
       <img
-        src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
-        alt='poster comic'
+        src={`${element.thumbnail.path}.${element.thumbnail.extension}`}
+        alt='poster element'
       />
       <div className='container-title-description'>
-        <h2>{comic.title}</h2>
-        <h3>{comic.description}</h3>
+        <h2>
+          {currentURLPathname === '/characters'
+            ? element.name
+            : currentURLPathname === '/comics'
+            ? element.title
+            : ''}
+        </h2>
+        <h3>{element.description}</h3>
       </div>
 
       <div className='btn'>Ajouter aux coups de coeur</div>
