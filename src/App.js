9887966ba1dca651fabec73da7a6f8c89a75e242
Farid <bouras.farid@outlook.fr>
2022-08-07 11:14:43 +0200

    build: added a new page "'/character/:id' "

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Comics from './pages/Comics';
 import Favorites from './pages/Favorites';
 import Characters from './pages/Characters';
+import CharacterDetails from './pages/CharacterDetails';
 import Video from './components/Video';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/characters' element={<Characters />} />
+        <Route path='/character/:id' element={<CharacterDetails />} />
         <Route path='/comics' element={<Comics />} />
         <Route path='/favorites' element={<Favorites />} />
         <Route path='*' element={<Video />} />
